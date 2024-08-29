ad3bf428aa8987bb24fb5065e513fcc77c347080
Denis Corbin <dar.linux@free.fr>
2024-08-29 16:23:29 +0200

    fixing typo in licensing info

--- Examples/translate.py
+++ Examples/translate.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 ########################################################################
-# ray-install.sh - a simple script to install Ray on Linux
+# translate.py - a simple script to install Ray on Linux
 # Copyright (C) 2024 Denis Corbin
 #
-#  ray-install.sh is free software: you can redistribute it and/or modify
+#  translate.py is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
 #
-#  ray-install.sh is distributed in the hope that it will be useful,
+#  translate.py is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
